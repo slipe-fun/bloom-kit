c6c63d88079b3d7935eb4b409a63a4375323e765
pip64 <76626915+pip64@users.noreply.github.com>
2026-07-28 02:14:24 +0700

    Add loginned user listener

--- examples/client/main.go
+++ examples/client/main.go
@@ -16,8 +16,23 @@ type MessagesHandler struct {
 	client *client.BloomClient
 }
 
+type UserHandler struct{}
+
 func (h *MessagesHandler) OnNewMessage(messageJSON []byte) {
 	fmt.Println("New message:", string(messageJSON))
+}
+
+func (h *UserHandler) OnUserUpdated(userJSON []byte) {
+	var user *domain.User
+	if err := json.Unmarshal(userJSON, &user); err != nil {
+		return
+	}
+
+	fmt.Println("User updated")
+	fmt.Println("User id:", user.ID)
+	fmt.Println("User username:", user.Username)
+	fmt.Println("User display name:", user.DisplayName)
+	fmt.Println("User description:", user.Description)
 }
 
 func main() {
@@ -27,28 +42,45 @@ func main() {
 	// 	panic(err)
 	// }
 
-	client := client.NewClient("https://api.bloomapp.pw/", "wss://api.bloomapp.pw/ws", "./storage", key)
-	if err := client.Initialize(); err != nil {
+	bloomClient := client.NewClient("https://api.bloomapp.pw/", "wss://api.bloomapp.pw/ws", "./storage", key)
+	if err := bloomClient.Initialize(); err != nil {
 		panic(err)
 	}
 
-	client.Register()
+	bloomClient.RegisterMessagesListener(&MessagesHandler{
+		client: bloomClient,
+	})
 
-	userJSON, err := client.GetMe()
-	if err != nil {
-		panic(err)
-	}
+	bloomClient.RegisterUserListener(&UserHandler{})
+
+	registerResponse, err := bloomClient.Register()
+	// recoveryKey := ""
+	// a, err := bloomClient.Login(recoveryKey)
+
+	// userJSON, err := bloomClient.GetMe()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	var user *domain.User
-	if err = json.Unmarshal(userJSON, &user); err != nil {
+	if err := json.Unmarshal(registerResponse.UserJSON, &user); err != nil {
 		panic(err)
 	}
 
 	fmt.Println(user.ID)
 
-	client.RegisterMessagesListener(&MessagesHandler{
-		client: client,
-	})
+	editUserRequest := &client.EditRequest{}
+	editUserRequest.Username = "coolusername"
+	editUserRequest.HasUsername = true
+	editUserRequest.DisplayName = "cooldisplay"
+	editUserRequest.HasDisplayName = true
+	editUserRequest.Description = "description description coool"
+	editUserRequest.HasDescription = true
+
+	_, err = bloomClient.EditUser(editUserRequest)
+	if err != nil {
+		panic(err)
+	}
 
 	// roomID, fingerprint, err := client.StartExchangeSession("push")
 	// if err != nil {
